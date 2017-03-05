24a709caf2e9faddf57c330b21200c5062b73ed3
Stefan Mayr <stefanmayr@2a02-8388-e281-f000-5ab0-35ff-fe7f-e669.cable.dynamic.v6.surfer.at>
2017-03-05 14:38:08 +0100

    Initial Commit

---
+++ AppShare/AppShare.h
@@ -0,0 +1,19 @@
+//
+//  AppShare.h
+//  AppShare
+//
+//  Created by Stefan Mayr on 05.03.17.
+//  Copyright © 2017 Grizzly New Technologies GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppShare.
+FOUNDATION_EXPORT double AppShareVersionNumber;
+
+//! Project version string for AppShare.
+FOUNDATION_EXPORT const unsigned char AppShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppShare/PublicHeader.h>
+
+
